8f614dff0c457c5b26b2d4f2efe1a445097573d4
CapperTheRapper <casper@mulleland.dk>
2024-11-18 13:16:12 +0100

    Changed the rest of names to be of the correct format

--- Filter.py
+++ Filter.py
@@ -1,32 +1,32 @@
 from dataclasses import dataclass
-import Comparator
-import Network
+import comparator
+import network
 
 @dataclass
 class Filter:
     """
         Class that contains a network of comparators as well as all the binary outputs of said network
     """
-    netw: Network.Network
+    netw: network.Network
     binaryOut: list[list[int]]
 
-def makeEmptyFilter(n: int) -> Filter:
+def make_empty_filter(n: int) -> Filter:
     """
         Creates a new instance of Filter where the network is empty and the binary outputs are of length n
         
         n must be greater than 1
 
-        >>> makeEmptyFilter(3)
+        >>> make_empty_filter(3)
         Filter(netw=Network(comparators=[]), binaryOut=[[0, 0, 0], [0, 0, 1], \
 [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
     """
-    return Filter(Network.emptyNetwork(), Network.allOutputs(Network.emptyNetwork(), n))
+    return Filter(network.empty_network(), network.all_outputs(network.empty_network(), n))
 
-def net(f: Filter) -> Network.Network:
+def net(f: Filter) -> network.Network:
     """
         Returns the network in filter f
 
-        >>> net(makeEmptyFilter(3))
+        >>> net(make_empty_filter(3))
         Network(comparators=[])
     """
     return f.netw
@@ -35,71 +35,71 @@ def out(f: Filter) -> list[list[int]]:
     """
         Returns the binary outputs of filter f
 
-        >>> out(makeEmptyFilter(3))
+        >>> out(make_empty_filter(3))
         [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], \
 [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
     """
     return f.binaryOut
 
-def tempNet(c: Comparator.Comparator) -> Network.Network:
+def temp_net(c: comparator.Comparator) -> network.Network:
     """
         Returns a network with only one element, namely c
 
         Used as helping function in multiple functions
 
-        >>> tempNet(Comparator.makeComparator(0,1))
+        >>> temp_net(comparator.make_comparator(0,1))
         Network(comparators=[Comparator(channel1=0, channel2=1)])
     """
-    return Network.append(c, Network.emptyNetwork())
+    return network.append(c, network.empty_network())
 
-def isRedundant(c: Comparator.Comparator, f: Filter) -> bool:
+def is_redundant(c: comparator.Comparator, f: Filter) -> bool:
     """
         Returns true if the comparator c is redundant according to f
         Returns false if not
         i.e. returns true if the length of the binary outputs of f doesn't change if c is applied to it
 
-        >>> filt = makeEmptyFilter(3)
-        >>> comp = Comparator.makeComparator(0,1)
-        >>> isRedundant(comp, filt)
+        >>> filt = make_empty_filter(3)
+        >>> comp = comparator.make_comparator(0,1)
+        >>> is_redundant(comp, filt)
         False
 
         >>> filt = add(comp, filt)
-        >>> isRedundant(comp, filt)
+        >>> is_redundant(comp, filt)
         True
     """
-    return len(out(f)) == len(Network.outputs(tempNet(c), out(f)))
+    return len(out(f)) == len(network.outputs(temp_net(c), out(f)))
 
-def add(c: Comparator.Comparator, f: Filter) -> Filter:
+def add(c: comparator.Comparator, f: Filter) -> Filter:
     """
         Updates filter f by adding comparator c
         i.e. adds a comparator c to the network in f and updates binaryOut in f to reflect this change
 
-        maxChannel of the comparator c must be less than the length of each element in f.binaryOut
+        max_channel of the comparator c must be less than the length of each element in f.binaryOut
 
-        >>> filt = makeEmptyFilter(3)
-        >>> comp = Comparator.makeComparator(0,1)
+        >>> filt = make_empty_filter(3)
+        >>> comp = comparator.make_comparator(0,1)
         >>> add(comp, filt)
         Filter(netw=Network(comparators=[Comparator(channel1=0, channel2=1)]), binaryOut=[[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 1, 0], [1, 1, 1]])
     """
-    newFilt = Filter(Network.append(c, net(f)), Network.outputs(tempNet(c), out(f)))
+    newFilt = Filter(network.append(c, net(f)), network.outputs(temp_net(c), out(f)))
     return newFilt
 
 
-def isSorting(f: Filter) -> bool:
+def is_sorting(f: Filter) -> bool:
     """
         Returns true if the network sorts each binary input and false if not
         Uses the fact that there are n + 1 more lists in a sorted list of lists with n being the amount of element in the inner lists
 
-        >>> filt = makeEmptyFilter(3)
-        >>> comp = Comparator.makeComparator(0,1)
+        >>> filt = make_empty_filter(3)
+        >>> comp = comparator.make_comparator(0,1)
         >>> filt = add(comp, filt)
-        >>> isSorting(filt)
+        >>> is_sorting(filt)
         False
 
-        >>> filt = makeEmptyFilter(2)
-        >>> comp = Comparator.makeComparator(0,1)
+        >>> filt = make_empty_filter(2)
+        >>> comp = comparator.make_comparator(0,1)
         >>> filt = add(comp, filt)
-        >>> isSorting(filt)
+        >>> is_sorting(filt)
         True
     """
     return len(out(f)) == len(out(f)[0]) + 1
--- generate.py
+++ generate.py
@@ -1,23 +1,23 @@
-import Filter
-import Network
-import Comparator
+import filter
+import network
+import comparator
 
 
-def extend(w: list[Filter], n: int) -> list[Filter]:
+def extend(w: list[filter.Filter], n: int) -> list[filter.Filter]:
     """
 
     """
     result = []
-    stdComparators = Comparator.stdComparators(n)
+    std_comparators = comparator.std_comparators(n)
     for f in w:
-        for c in stdComparators:
-            if not Filter.isRedundant(c, f):
-                result = result + [Filter.add(c, f)]
+        for c in std_comparators:
+            if not filter.is_redundant(c, f):
+                result = result + [filter.add(c, f)]
     return result
     
     result = []
-    stdComparators = Comparator.stdComparators(n)
+    std_comparators = comparator.std_comparators(n)
     for f in w:
-        nonRedundants = list(filter(lambda x: not Filter.isRedundant(x, f), stdComparators))
-        result = result + list(map(lambda x: Filter.add(x, f), nonRedundants))
+        nonRedundants = list(filter(lambda x: not filter.is_redundant(x, f), std_comparators))
+        result = result + list(map(lambda x: filter.add(x, f), nonRedundants))
     return result
