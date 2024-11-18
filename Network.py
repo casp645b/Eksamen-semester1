7f34be781ad1ae217ff4b79ea3e9f11cf030da82
CapperTheRapper <casper@mulleland.dk>
2024-11-18 12:23:45 +0100

    Changed names of functions in network module to correct format

--- Network.py
+++ Network.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-import Comparator
+import comparator
 import functools
 
 @dataclass
@@ -7,24 +7,24 @@ class Network:
     """
         Class that contains a list of comparators to use
     """
-    comparators: list[Comparator.Comparator]
+    comparators: list[comparator.Comparator]
 
 
-def emptyNetwork() -> Network:
+def empty_network() -> Network:
     """
         Creates a new empty instance of network
 
-        >>> emptyNetwork()
+        >>> empty_network()
         Network(comparators=[])
     """
     return Network([])
 
 
-def append(c: Comparator.Comparator, net: Network) -> Network:
+def append(c: comparator.Comparator, net: Network) -> Network:
     """
         Adds a comparator to the end of the network
 
-        >>> append(Comparator.makeComparator(0, 1), emptyNetwork())
+        >>> append(comparator.make_comparator(0, 1), empty_network())
         Network(comparators=[Comparator(channel1=0, channel2=1)])
     """
     newNet = Network(net.comparators.copy())
@@ -35,43 +35,43 @@ def size(net: Network) -> int:
     """
         Returns the number of comparators in the network
 
-        >>> size(append(Comparator.makeComparator(0, 1), emptyNetwork()))
+        >>> size(append(comparator.make_comparator(0, 1), empty_network()))
         1
     """
     return len(net.comparators)
 
-def maxChannel(net: Network) -> int:
+def max_channel(net: Network) -> int:
     """
         Returns the largest channel the network can affect
 
         Function cannot take empty networks
 
-        >>> maxChannel(append(Comparator.makeComparator(0, 1), emptyNetwork()))
+        >>> max_channel(append(comparator.make_comparator(0, 1), empty_network()))
         1
     """
-    return max(map(Comparator.maxChannel, net.comparators))
+    return max(map(comparator.max_channel, net.comparators))
 
 
-def isStandard(net: Network) -> bool:
+def is_standard(net: Network) -> bool:
     """
         Returns true if all comparators in the network are "Standard"
         else returns false
 
-        >>> isStandard(append(Comparator.makeComparator(0, 1), emptyNetwork()))
+        >>> is_standard(append(comparator.make_comparator(0, 1), empty_network()))
         True
     """
-    return list(filter(Comparator.isStandard, net.comparators)) == net.comparators
+    return list(filter(comparator.is_standard, net.comparators)) == net.comparators
 
 
 def apply(net: Network, w: list[int]) -> list[int]:
     """
         Applies the comparators in a network to the list w
 
-         the length of w has to greater than maxChannel of net
-        >>> apply(append(Comparator.makeComparator(0, 1), emptyNetwork()), [3, 2, 1])
+         the length of w has to greater than max_channel of net
+        >>> apply(append(comparator.make_comparator(0, 1), empty_network()), [3, 2, 1])
         [2, 3, 1]
     """
-    return list(functools.reduce(lambda x, y: Comparator.apply(y, x),
+    return list(functools.reduce(lambda x, y: comparator.apply(y, x),
                                  net.comparators, w))
 
 
@@ -80,10 +80,10 @@ def outputs(net: Network, w: list[list[int]]) -> list[list[int]]:
         Returns a list of unique lists (i.e. removes duplicates)
         as a result of net being applied to the lists in w
 
-        the length of each element of w must be greater than maxChannel of net
+        the length of each element of w must be greater than max_channel of net
 
-        >>> outputs(append(Comparator.makeComparator(0, 1),
-        ... emptyNetwork()), [[3, 2, 1], [2, 3, 1], [2, 1, 3]])
+        >>> outputs(append(comparator.make_comparator(0, 1),
+        ... empty_network()), [[3, 2, 1], [2, 3, 1], [2, 1, 3]])
         [[2, 3, 1], [1, 2, 3]]
     """
     v = list(map(lambda x: apply(net, x), w))
@@ -101,21 +101,21 @@ def member(x: any, v: list) -> bool:
     return list(filter(lambda y: x == y, v)) != []
 
 
-def allOutputs(net: Network, n: int) -> list[list[int]]:
+def all_outputs(net: Network, n: int) -> list[list[int]]:
     """
         Returns the output of the outputs function
         for all binary permutations of a list with length n
 
-        n has to be greater than maxChannel of net
+        n has to be greater than max_channel of net
 
-        >>> allOutputs(append(Comparator.makeComparator(0, 1), emptyNetwork()), 3)
+        >>> all_outputs(append(comparator.make_comparator(0, 1), empty_network()), 3)
         [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 1, 0], [1, 1, 1]]
     """
-    v = list(map(lambda x: baseTenToBinary(x, n), range(2**n)))
+    v = list(map(lambda x: base_ten_to_binary(x, n), range(2**n)))
     return outputs(net, v)
 
 
-def baseTenToBinary(m: int, n: int) -> list[int]:
+def base_ten_to_binary(m: int, n: int) -> list[int]:
     """
         Converts a base ten number m to a list reprecenting its binary value
         of length n
@@ -123,14 +123,14 @@ def baseTenToBinary(m: int, n: int) -> list[int]:
         
         m must be less than 2^n
         
-        >>> baseTenToBinary(29, 7)
+        >>> base_ten_to_binary(29, 7)
         [0, 0, 1, 1, 1, 0, 1]
     """
     v = list(map(lambda x: pow(2, x), range(n-1, -1, -1)))
     return list(map(lambda x: (m//v[x])%2, range(n)))
 
 
-def isSorting(net: Network, size: int) -> bool:
+def is_sorting(net: Network, size: int) -> bool:
     """
         Check whether or not the comparator network net 
         is a sorting network to size inputs,
@@ -141,39 +141,39 @@ def isSorting(net: Network, size: int) -> bool:
         size must be greater than the maximum channel the network refers to 
         for this function to be used
 
-        >>> isSorting(append(Comparator.makeComparator(0, 1), emptyNetwork()), 3)
+        >>> is_sorting(append(comparator.make_comparator(0, 1), empty_network()), 3)
         False
 
-        >>> isSorting(append(Comparator.makeComparator(0, 1), emptyNetwork()), 2)
+        >>> is_sorting(append(comparator.make_comparator(0, 1), empty_network()), 2)
         True
     """
-    return len(allOutputs(net, size)) == size + 1
+    return len(all_outputs(net, size)) == size + 1
 
 
-def toProgram(net: Network, var: str, aux: str) -> list[str]:
+def to_program(net: Network, var: str, aux: str) -> list[str]:
     """
         Returns a list of strings that, if inserted in  Python shell,
         sorts a list with name var based on instructions in network
 
         Does not use the variable aux
 
-        >>> toProgram(append(Comparator.makeComparator(0, 1), emptyNetwork()),
+        >>> to_program(append(comparator.make_comparator(0, 1), empty_network()),
         ... "a", "b")
-        ['c = makeComparator(0, 1)', 'i = 0', 'j = 1', 'if a[i] < a[j]:', \
-'  if not isStandard(c):', '    a[i], a[j] = a[j], a[i]', 'else:', \
-'  if isStandard(c):', '    a[i], a[j] = a[j], a[i]']
+        ['c = make_comparator(0, 1)', 'i = 0', 'j = 1', 'if a[i] < a[j]:', \
+'  if not is_standard(c):', '    a[i], a[j] = a[j], a[i]', 'else:', \
+'  if is_standard(c):', '    a[i], a[j] = a[j], a[i]']
 
-        >>> toProgram(append(Comparator.makeComparator(1, 2),
-        ... append(Comparator.makeComparator(0, 1), emptyNetwork())), "a", "b")
-        ['c = makeComparator(0, 1)', 'i = 0', 'j = 1', 'if a[i] < a[j]:', \
-'  if not isStandard(c):', '    a[i], a[j] = a[j], a[i]', 'else:', \
-'  if isStandard(c):', '    a[i], a[j] = a[j], a[i]', 'c = makeComparator(1, 2)', \
-'i = 1', 'j = 2', 'if a[i] < a[j]:', '  if not isStandard(c):', \
-'    a[i], a[j] = a[j], a[i]', 'else:', '  if isStandard(c):', \
+        >>> to_program(append(comparator.make_comparator(1, 2),
+        ... append(comparator.make_comparator(0, 1), empty_network())), "a", "b")
+        ['c = make_comparator(0, 1)', 'i = 0', 'j = 1', 'if a[i] < a[j]:', \
+'  if not is_standard(c):', '    a[i], a[j] = a[j], a[i]', 'else:', \
+'  if is_standard(c):', '    a[i], a[j] = a[j], a[i]', 'c = make_comparator(1, 2)', \
+'i = 1', 'j = 2', 'if a[i] < a[j]:', '  if not is_standard(c):', \
+'    a[i], a[j] = a[j], a[i]', 'else:', '  if is_standard(c):', \
 '    a[i], a[j] = a[j], a[i]']
     """
     return list(functools.reduce(lambda x, y: x+y,
-                                 map(lambda x: Comparator.toProgram(x, var, aux),
+                                 map(lambda x: comparator.to_program(x, var, aux),
                                      net.comparators), []))
 
 if __name__ == "__main__":
