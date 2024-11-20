4cabd7e940538a5484ec9781c88ead4e247eae9f
CapperTheRapper <casper@mulleland.dk>
2024-11-20 17:28:39 +0100

    Made the rest of functions. Need to add interface and documentation.

--- network_finder.py
+++ network_finder.py
@@ -1,3 +1,14 @@
-import Network
+import network
+import filter
+import generate
+import prune
 
-while not Network.isSorting()
+def network_finder(n: int) -> network.Network:
+    """
+        Finds a sortingnetwork for n channels/inputs
+    """
+    filters = [filter.make_empty_filter(n)]
+    while not filter.is_sorting(filters[0]):
+        filters = generate.extend(filters, n)
+        filters = prune.prune(filters, n)
+    return filter.net(filters[0])
